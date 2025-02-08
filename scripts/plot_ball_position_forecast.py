599154ad3eaa41db3c021eff9ec8db3dc432aa3d
Nikita Pavlov <nikita.pavlov.sva@hotmail.com>
2025-02-08 13:44:28 +0100

    made it possible to plot the predicted ball position with the player positions

--- scripts/plot_ball_position_forecast.py
+++ scripts/plot_ball_position_forecast.py
@@ -25,13 +25,17 @@ if train_or_prediction_data=="prediction":
 
     prediction_df = pd.read_pickle(ball_prediction_pickle_file)
 
-    full_df = pd.concat([test_data[["IdPeriod","Time"]],prediction_df],axis=1)
+    full_df = pd.concat([test_data,prediction_df],axis=1)
 elif train_or_prediction_data=="train":
     #collect the train_data for specified match_id
     train_data = load_data.split_train_test_data(match_dict=aggregated_match_dict,match_ids=[training_match_id])
     print(f"testing dataset created")
 
-    full_df = train_data[["IdPeriod","Time","ball_x","ball_y"]]
-    full_df.columns = ["IdPeriod","Time","ball_x_pred","ball_y_pred"]
+    full_df = train_data.rename(columns={"ball_x":"ball_x_pred","ball_y":"ball_y_pred"})
 
+plot_ball_trajectory_df = full_df.copy()[["IdPeriod","Time","ball_x_pred","ball_y_pred"]]
 load_data.plot_ball_trajectory(full_df)
+
+full_df = full_df.rename(columns={"ball_x_pred":"ball_x","ball_y_pred":"ball_y"})
+
+load_data.plot_all_players_in_timestamp(id_half=1,time=10000,match_df=full_df)
